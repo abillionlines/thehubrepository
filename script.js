3bff73807031c05977cbff620030606993f719e6
tenlegends <tenlegends@MacBook-Pro-2.local>
2025-12-01 15:34:42 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+document.querySelector('.title').innerHtml = 'Brian Wilkinson'
+document.querySelector(".title").style.fontSize = '16px'
+
+function redchange(document.querySelector(".buttons").style.color = 'red')
+{
+ 
+
+  console.log('clicked')
+}
